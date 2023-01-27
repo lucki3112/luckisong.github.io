135842cc83fae2751342574985b79e829a71465e
Sk Lucki Hossain <skluckihossain@gmail.com>
2023-01-27 18:34:17 +0530

    file added

---
+++ index.js
@@ -0,0 +1,186 @@
+let mangi_play = new Audio(
+  "/all songs/Mangi Mangi Dua Mangi Hai-Arijit Singh.mp3"
+);
+let teri_yado_play = new Audio("/all songs/Teri Yaadon Mein- Yasser Desai.mp3");
+let like_you_play = new Audio(
+  "/all songs/Like You Be Strong ! Superhero - Unknown Brain.mp3"
+);
+let life_is_play = new Audio(
+  "/all songs/Life Is Drink - Hynm For The Weekend ! BGM.mp3"
+);
+let Chhichhore_play = new Audio(
+  "/all songs/Chhichhore - Friendship Dialogue.mp3"
+);
+let bin_tere_sanam_play = new Audio(
+  "/all songs/BIN-TERE-SANAM-AY-REMIX-11.mp3"
+);
+let Jhume_jo_pathan_play = new Audio(
+  "/all songs/Jhoome Jo Pathaan meri jaan - Jhoome Jo Pathaan ! Pathaan.mp3"
+);
+let vikram_play = new Audio(
+  "/all songs/Vikram - Vijay Sethupathi Entry Bgm __CaronTunes_Full-HD.mp3"
+);
+function mangi() {
+  mangi_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#mangi_play").hide();
+    $("#mangi_pause").show();
+    $("#mangi_pause").on("click", function () {
+      mangi_play.pause();
+      $("#gif").hide();
+      $("#gif").hide();
+      $("#mangi_play").show();
+      $("#mangi_pause").hide();
+    });
+  });
+}
+function teri_yado() {
+  teri_yado_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#teri_yado_play").hide();
+    $("#teri_yado_pause").show();
+    $("#teri_yado_pause").on("click", function () {
+      teri_yado_play.pause();
+      $("#gif").hide();
+      $("#teri_yado_play").show();
+      $("#teri_yado_pause").hide();
+    });
+  });
+}
+function like_you() {
+  like_you_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#like_you_play").hide();
+    $("#like_you_pause").show();
+    $("#like_you_pause").on("click", function () {
+      like_you_play.pause();
+      $("#gif").hide();
+      $("#like_you_play").show();
+      $("#like_you_pause").hide();
+    });
+  });
+}
+function life_is() {
+  life_is_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#life_is_play").hide();
+    $("#life_is_pause").show();
+    $("#life_is_pause").on("click", function () {
+      life_is_play.pause();
+      $("#gif").hide();
+      $("#life_is_play").show();
+      $("#life_is_pause").hide();
+    });
+  });
+}
+function Chhichhore() {
+  Chhichhore_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#Chhichhore_play").hide();
+    $("#Chhichhore_pause").show();
+    $("#Chhichhore_pause").on("click", function () {
+      Chhichhore_play.pause();
+      $("#gif").hide();
+      $("#Chhichhore_play").show();
+      $("#Chhichhore_pause").hide();
+    });
+  });
+}
+function bin_tere_sanam() {
+  bin_tere_sanam_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#bin_tere_sanam_play").hide();
+    $("#bin_tere_sanam_pause").show();
+    $("#bin_tere_sanam_pause").on("click", function () {
+      bin_tere_sanam_play.pause();
+      $("#gif").hide();
+      $("#bin_tere_sanam_play").show();
+      $("#bin_tere_sanam_pause").hide();
+    });
+  });
+}
+function Jhume_jo_pathan() {
+  Jhume_jo_pathan_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#Jhume_jo_pathan_play").hide();
+    $("#Jhume_jo_pathan_pause").show();
+    $("#Jhume_jo_pathan_pause").on("click", function () {
+      Jhume_jo_pathan_play.pause();
+      $("#gif").hide();
+      $("#Jhume_jo_pathan_play").show();
+      $("#Jhume_jo_pathan_pause").hide();
+    });
+  });
+}
+function vikram() {
+  vikram_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#vikram_play").hide();
+    $("#vikram_pause").show();
+    $("#vikram_pause").on("click", function () {
+      vikram_play.pause();
+      $("#gif").hide();
+      $("#vikram_play").show();
+      $("#vikram_pause").hide();
+    });
+  });
+}
+let phir_na_aisi_play = new Audio(
+  "/all songs/Phir Na Aisi Raat Aayegi - Arijit Singh ! Hindi.mp3"
+);
+function phir_na_aisi() {
+  phir_na_aisi_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#phir_na_aisi_play").hide();
+    $("#phir_na_aisi_pause").show();
+    $("#phir_na_aisi_pause").on("click", function () {
+      phir_na_aisi_play.pause();
+      $("#gif").hide();
+      $("#phir_na_aisi_play").show();
+      $("#phir_na_aisi_pause").hide();
+    });
+  });
+}
+let na_bhula_saka_play = new Audio(
+  "/all songs/Na bhula saka - Dil ! Ek Villain Returns.mp3"
+);
+function na_bhula_saka() {
+  na_bhula_saka_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#na_bhula_saka_play").hide();
+    $("#na_bhula_saka_pause").show();
+    $("#na_bhula_saka_pause").on("click", function () {
+      na_bhula_saka_play.pause();
+      $("#gif").hide();
+      $("#na_bhula_saka_play").show();
+      $("#na_bhula_saka_pause").hide();
+    });
+  });
+}
+let bazigar_play = new Audio(
+  "/all songs/Baazigar - DIVINE ! Gunehgar ! Armani White.mp3"
+);
+function bazigar() {
+  bazigar_play.play();
+  $("#gif").show();
+  $(document).ready(function () {
+    $("#bazigar_play").hide();
+    $("#bazigar_pause").show();
+    $("#bazigar_pause").on("click", function () {
+      bazigar_play.pause();
+      $("#gif").hide();
+      $("#bazigar_play").show();
+      $("#bazigar_pause").hide();
+    });
+  });
+}
